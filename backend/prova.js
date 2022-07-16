9fd8c15446b3b98826e2e6f4df3e4d132ca37ea2
Dario Busacca <57256390+DarioBusacca@users.noreply.github.com>
2022-07-16 15:55:24 +0200

    Update prova.js

--- backend/prova.js
+++ backend/prova.js
@@ -1,34 +1,96 @@
-const {google} = require('googleapis');
-var express = require('express');
-var path = require('path');
 
+// preso a_t devo chiedere il nome del canale o l'id e poi provare ad avere una lista dei video
+const {google} = require('googleapis');
+var express=require('express');
+var request = require('request');
+var path = require('path');
+var app= express();
 const fs = require('fs');
 const multer=require('multer');
 const { title } = require('process');
-
 var cred = fs.readFileSync('./credenziali.json');
-
-const app = express();
-
-
+const WebSocket = require('ws');
 //npm install pg
 const {Client} = require('pg');
-const { response } = require('express');
-const { oauth2 } = require('googleapis/build/src/apis/oauth2');
+const bodyParser = require('body-parser');
+var quanti_tweet=8;
+app.use(
+    bodyParser.urlencoded({
+      extended: false,
+    })
+  )
+  
+app.use(bodyParser.json());
+
+
+const lista = new Array();
+class video {
+	constructor(id,titolo,desc,data) { 
+		this.videoId = id;
+		this.title = titolo;
+		this.description = desc;
+		this.time = data;
+	}
+    Stampa_cont(){
+        console.log( "video id == "+this.videoId + " video title == " + this.title+ " description == " + this.description + " published time == " + this.data);
+}
+	
+	// get email() { return this._email; }
+	
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+const ws = new WebSocket.Server({ port: 9998 });
+
+
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
-    database: 'rdc',
-    password: 'postgres',
+    database: 'rdc_1',
+    // password: 'postgres',
+    password: 'ciaodafrancesca',
     port: 5432
-});
-client.connect();
+  });
 
+  ws.on('connection', function connection(ws) {
 
+    console.log("new client connected");
+    // ws.send("hello new client");
+  
+  
+    ws.on('message', function incoming(message) {
+       
+      console.log('received_s: %s', message);
+      ws.send("the message i receiver is"+message);
+  
+      if(message=="connect"){
+        client.connect();
+  
+      }
+      if(message=="query"){
+        client.connect();
+        const query = "SELECT * FROM azioni "
+        client
+                    .query(query)
+                    .then(res => {
+                        console.log(res.rows[0])
+                        
+                    })
+                    .catch(e => console.error(e.stack))
+  
+      }
+    });
+  
+   
+  
+  });
+///////////////////////////////////////////////////////////////////////////////////////////
 var sec = JSON.parse(cred);
-console.log(sec);
+// console.log(sec);
 
 // console.log(sec);
+// const identificativo_canale="UCVuZe4vjCbQLCLLeXW2NH_Q";
+// var channel_id;
+// var a_t;
 const client_id = sec.web.client_id;
 const client_secret = sec.web.client_secret;
 const red_uri=sec.web.redirect_uris[0];
@@ -40,7 +102,15 @@ const oAuth2Client=new google.auth.OAuth2(
     client_secret,
     red_uri
 );
+// console.log("questo è il contenuto dopo google.auth.oauth");
+// console.log(oAuth2Client);
+/*access_token: 
+    refresh_token: 
+    scope: 
+    token_type: 'Bearer',
+    id_token:*/
 
+// praticamente sposta il file nella cartella ./videos
 var Storage=multer.diskStorage({
     destination: function(req,file,callback){
         callback(null,"./videos");
@@ -51,15 +121,211 @@ var Storage=multer.diskStorage({
 })
 var upload= multer({
     storage: Storage,
-}).single("file");
+}).single("file");  //file.fieldname
+
 
 var autenticato=false;
-var scopes = "https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/userinfo.profile";
+var scopes = "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly";
 
-// app.get('/login', function(req, res){
-//     res.redirect("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/calendar.events&response_type=code&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri="+red_uri+"&client_id="+client_id);
-//   });
 
+app.get('/login',function(req,res){
+
+    res.render("pagina_0");
+
+
+})
+app.get('/home', function(req, res){
+    if(!autenticato){
+        // console.log("non autenticato");
+        var url=oAuth2Client.generateAuthUrl({
+            access_type: "offline",
+            scope:scopes,
+        });
+        //url = genera l'url per avere il token con gli scope richiesti
+        // console.log("oauth2client.generateurl");
+        // console.log(url);
+        res.redirect(url);
+        // res.render('index',{url:url});
+    }
+    else{
+        console.log("utente autenticato, /home");
+
+        var oauth2=google.oauth2({
+            version:'v2',
+            auth:oAuth2Client
+        })
+        
+        // console.log("oauth2Client\n");
+        // console.log(oAuth2Client);
+       
+        var prova=oAuth2Client.credentials;
+        // console.log("dovrei aver preso access token ecc");
+        // console.log(prova);
+        a_t=prova.access_token;
+        //unico campo rilevante di oauth2
+        oauth2.userinfo.get(function(err,response){
+            if(err) throw err
+            // console.log(response.data);
+            var name=response.data.name;
+            // channel_id=response.data.id;
+            
+
+            res.render("home_youtube",{name:name});
+        })
+        
+
+    }
+});
+//&key=AIzaSyAwlCGZ0GWGuB6pUgSsSvqbMVUQRlEMMDU
+    
+
+
+app.get('/videos',(req,res,body)=>{
+
+    console.log("sono dentro /videos e voglio chiamare fnz\n");
+    // console.log(oAuth2Client);
+
+    const youtube_1 =google.youtube({
+        version: 'v3',
+        auth: oAuth2Client
+    })
+
+    youtube_1.search.list({
+
+        "part":[
+            "snippet"
+        ],
+        "forMine": true,
+        "type": [
+          "video"
+        ]
+
+    })
+    .then(function(response) {
+        // Handle the results here (response.result has the parsed body).
+        console.log("stampa la risposta del server\n\n");
+        //console.log("Response", response);
+        var vid=response.data.items;
+        console.log("da qui lista video??");
+        //console.log(vid);
+        
+        // console.log("quanti video (lunghezza l) ",l);
+        var i=0;
+        for (const v of vid) {
+            
+            var video_item = new video(v.id.videoId,v.snippet.title,v.snippet.description,v.snippet.publishedAt);
+            video_item.Stampa_cont();
+            lista.push(video_item);
+            
+          }
+          console.log(lista);
+        
+
+        res.render("lista_video",{lista:lista});
+        // res.render('index',{url:url});
+      
+    
+        console.log("\n\nfine elenco\n\n");
+      },
+      function(err) { console.error("Execute error", err); },);
+
+
+
+})
+app.post('/prova',(req,res)=>{
+    console.log(req.body);
+})
+app.post('/delete',(req,res)=>{
+
+    
+    console.log("\nsono dentro /delete e voglio chiamare fnz\n");
+    
+   
+    console.log(req.body);
+
+    // var b=res.json({requestBody: req.body.title_v})
+    // console.log(b)
+
+    var id_video=req.body.identif_video;
+    console.log(id_video);
+
+
+    const youtube_d =google.youtube({
+        version: 'v3',
+        auth: oAuth2Client
+    })
+
+    youtube_d.videos.delete({
+
+        "id": id_video
+
+    })
+    .then(function(response) {
+        // Handle the results here (response.result has the parsed body).
+        // console.log("Response", response);
+        console.log("\n\nfine cancellazione\n\n");
+      },
+      function(err) { console.error("Execute error", err); });    
+})
+
+app.post('/upload',(req,res)=>{
+    //metto il file in videos e chiamo callback fnz
+    upload(req,res,function(err){
+        if(err) throw err;
+        console.log(req.file.path);
+        var title=req.body.title;
+        var description = req.body.description;
+        var tags = req.body.tags;
+        // console.log("questo è il contenuto di google.auth.oauth dopo upload");
+        // console.log(oAuth2Client);
+        const youtube =google.youtube({
+            version: 'v3',
+            auth: oAuth2Client
+        })
+        console.log("youtube in upload");
+        // console.log(youtube);
+
+        // chiamo youtube api
+        youtube.videos.insert(
+            {
+            resource:{
+                snippet:{
+                    title:title,
+                    description:description,
+                    tags:tags
+                },
+                status:{
+                    privacystatus:"public"
+                },
+            },
+            part:"snippet,status",
+            media:{
+                 body:fs.createReadStream(req.file.path)
+            }
+            },
+            (err,data) => {
+                if(err) throw err
+                console.log("uploading video");
+                // res.render
+
+            }
+            
+        )
+        // .then(function(response) {
+        
+        //     console.log("video caricato ", response);
+        //     res.render("home_youtube");
+        //   },
+        //   function(err) { console.error("Execute error", err); });
+        
+    })
+    res.render("home_youtube");
+})
+
+
+app.get('/upload_form',(req,res)=>{
+    res.render('index_a');
+  })
 
 app.get('/',(req,res)=>{
     const code=req.query.code;
@@ -75,106 +341,14 @@ app.get('/',(req,res)=>{
 })
 
 
-//variabile globale per username da cercare nel database
-var nome = "";
+/////////                                     DA QUI TWITTER 
 
-app.get('/home', function(req, res){
-    if(!autenticato){
-        console.log("non autenticato");
-        var url=oAuth2Client.generateAuthUrl({
-            access_type: "offline",
-            scope:scopes,
-        });
-        console.log(url);
-        res.redirect(url);
-        // res.render('index',{url:url});
-    }
-    else{
-        console.log("utente autenticato, /home");
-
-        var oauth2=google.oauth2({
-            version:'v2',
-            auth:oAuth2Client
-        })
-        oauth2.userinfo.get(function(err,response){
-            if(err) throw err
-            console.log(response.data);
-            nome=response.data.name;
-
-            res.render("index_a",{name:nome});
-
-            const query = "INSERT INTO utenti (id_utente) SELECT * FROM (SELECT $1) AS tmp WHERE NOT EXISTS (SELECT id_utente FROM utenti WHERE id_utente = $1) RETURNING *"
-            const value = [nome]
-            
-            //callback
-            client.query(query, value, (err, res) => {
-                if(err) {
-                    console.log(err.stack)
-                } else{
-                console.log(res.rows[0]) //id_utente: name
-                }
-            });
-
-            //promise
-            client
-                .query(query, value)
-                .then(res => {
-                    console.log(res.rows[0])
-                    //{id_utente: name}
-                })
-                .catch(e => console.error(e.stack))
-        })
-    }
-});
-
-
-
-app.post('/upload',(req,res)=>{
-    upload(req,res,function(err){
-        if(err) throw err;
-        console.log(req.file.path)
-        // title=req.body.title
-
-        const youtube =google.youtube({
-            version: 'v3',
-            auth: oAuth2Client
-        })
-
-        // call youtube api
-        youtube.videos.insert(
-            {
-            resource:{
-                snippet:{
-                    title:"primo_video",
-                    description:"descrizione video",
-                    tags:"argomenti"
-                },
-                status:{
-                    privacystatus:"public"
-                },
-            },
-            part:"snippet,status",
-            media:{
-                 body:fs.createReadStream(req.file.path)
-            }
-            },
-            (err,data) => {
-                if(err) throw err
-                console.log("uploading video");
-
-            }
-        )
-    })
-});
-
-
-//TWITTER API
 var tw_auth = false;
-
+var utente_ver=0;
 const oauth = require('oauth');
-const got = require('got');
-const  get = require('request');
-const  request = require('http');
+// const got = require('got');
+// const  get = require('request');
+// const  request = require('http');
 var Twit = require('twit');
 
 var request_token_url = 'https://api.twitter.com/oauth/request_token';
@@ -210,7 +384,12 @@ function consumer(){
 }
 
 app.post('/twitter', (req, res) => {
-  res.redirect('/twitter/connect');
+    if(utente_ver){
+        res.render('twitter');
+    }
+    else{
+        utente_ver=1;
+  res.redirect('/twitter/connect');}
   });
 
 app.get('/twitter/connect', (req, res) => {
@@ -256,20 +435,55 @@ app.get('/twitter/callback', (req, res) => {
 
 
 app.post('/tweet', (req, res) => {
-
+  quanti_tweet=quanti_tweet+1;
+  var quale="Hello world"+"_"+quanti_tweet;
   const endpointURL = 'https://api.twitter.com/1.1/statuses/update.json';
-
+ 
   var T = new Twit({
     consumer_key: consumer_key,
     consumer_secret: consumer_secret,
     access_token: AccessToken,
     access_token_secret: AccessSecret
   });
-
-  T.post(endpointURL, {status: 'Hello world'}, function(err, data, response) {
+  console.log("\n\n");
+  console.log(req.body);
+  console.log(req.body.tweet_testo);
+  T.post(endpointURL, {status: quale}, function(err, data, response) {
     console.log(data);
   })
+  res.render("home_youtube");
   
 });
 
-app.listen(3000);
+
+app.get('/rimando',(req,res)=>{
+    res.render('twitter');
+})
+
+app.listen(3000,()=>console.log("server is listening on 3000"));
+
+
+
+
+
+
+
+
+
+
+
+/*
+per trovare id 
+
+GET https://youtube.googleapis.com/youtube/v3/channels?part=localizations&part=contentOwnerDetails&part=status&part=contentDetails&part=snippet&part=topicDetails&mine=true&key=[YOUR_API_KEY] HTTP/1.1
+
+Authorization: Bearer [YOUR_ACCESS_TOKEN]
+Accept: application/json
+*/
+/*
+GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&forMine=true&order=date&type=video&videoType=any&key=[YOUR_API_KEY] HTTP/1.1
+
+Authorization: Bearer [YOUR_ACCESS_TOKEN]
+Accept: application/json
+
+*/
